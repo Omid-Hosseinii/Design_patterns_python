c4703144e934f0dfc85df9b5e5f4114d71bd8f51
omid hosseini <omid.hamedhosseini@gmail.com>
2025-05-30 15:24:10 +0330

    decorator changes add git ignore file

--- Decorator_design_pattern/simple_decorator.py
+++ Decorator_design_pattern/simple_decorator.py
@@ -7,14 +7,12 @@ class TextComponent(ABC):
         pass
 
 
-
 class PlainText(TextComponent):
     def __init__(self, text):
         self.text = text
 
     def render(self):
         return self.text
-
 
 
 class TextDecorator(TextComponent):
@@ -28,11 +26,45 @@ class TextDecorator(TextComponent):
 class BoldDecorator(TextDecorator):
     def render(self):
         text = self.component.render()
-        return f"{text}: text became **bold**"
+        return f"<b>{text}</b>"
+
+
+class ItalicDecorator(TextDecorator):
+    def render(self):
+        text = self.component.render()
+        return f"<i>{text}</i>"
 
 
 
 text = PlainText("This my test text!")
 text = BoldDecorator(text)
-
+text = ItalicDecorator(text)
+print("Class-based decorator")
 print(text.render())
+print("-"*100)
+
+
+def bold_decorator(func):
+    def wrapper(*args, **kwargs):
+        text = func(*args, **kwargs)
+        changed_text = f"<b>{text}</b>"
+        return changed_text
+    return wrapper
+
+
+def italic_decorator(func):
+    def wrapper(*args, **kwargs):
+        text = func(*args, **kwargs)
+        changed_text = f"<i>{text}</i>"
+        return changed_text
+    return wrapper
+
+@italic_decorator
+@bold_decorator
+def my_text(text):
+    return text
+
+print("Pythonic decorator")
+print(my_text("hello world!"))
+print("-"*100)
+
